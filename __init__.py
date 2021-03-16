db405c4671ebe084183921a829401bbe7b0faca0
Zed Tan <zed@shootbird.work>
2021-03-16 04:53:40 +0100

    added docstrings; added todo in docstring

--- __init__.py
+++ __init__.py
@@ -3,6 +3,32 @@
 """
 Bad JSON rST tables parses a JSON files
 and spits out rST ``list-table`` s inelegantly.
+
+TODO:
+
+- Allow writing longform content to a single cell
+  by implementing a ipynb-like dict format.
+
+  E.g. parse the following:
+
+  ..  code-block::
+
+      "Assessment" : {
+          "h1" : "Heading 1",
+          "h2" : "Heading 2",
+          "ul-item" : "this is an unordered list item",
+          "ol-item" : "this is an ordered list item",
+          "p" : "this is a paragraph",
+          "code" : "print(\"this is a block of code (single-line)\")",
+          "code-block-item": "print(\"this is a single line in a code block\")",
+          "image" : "/img/sample.jpg"
+      }
+- Set filename write to from command line args.
+- By default, write to files named after the ``ID`` key in JSON. This is to capture the SA name as the filename.
+- Write proper example.json file.
+- Write tests
+- Take list of files or a dir from command line args.
+
 """
 
 import json
@@ -11,13 +37,18 @@ from enum import Enum
 
 FILENAME = os.path.join(os.path.abspath(os.path.curdir), 'data.json')
 
-class TableNodes(Enum):
-    INIT = '..  list-table::'
-    ATTR_HEADERROWS = '    :header-rows: 1'
-    ATTR_STUBCOLS   = '    :stub-columns: 1'
-    FIRST_ITEM  = '    - * '
-    ITEM        = '      * '
+class Nodes(Enum):
+    """Enum type for rst nodes"""
+    TABLE_INIT = '..  list-table::' #: Start of rst list-table node.
+    ATTR_HEADERROWS = '    :header-rows: 1' #: Node for the list-table attribute ``:header-rows: 1``. Always set to ``1``.
+    ATTR_STUBCOLS   = '    :stub-columns: 1' #: Node for the list-table attribute ``:stub-columns: 1``. Always set to ``1``.
+    STUB_ITEM  = '    - * ' #: Node for first item in a table row.
+    ITEM        = '      * ' #: Node for item in a table row.
     LEFTPAD     = '        '
+    """Pads 6 \s characters. Used to pad content in tables so that
+they match the indentation of their parent table nodes.
+Works only if the root table is at col 0
+(told ya this is an inelegant repo)."""
 
 def render_table(json_data: str) -> str:
     """
@@ -28,9 +59,9 @@ def render_table(json_data: str) -> str:
         json_data (str): Expects a JSON string.
     """
 
-    table_head = f"{TableNodes.INIT.value}\n" \
-        f"{TableNodes.ATTR_HEADERROWS.value}\n" \
-        f"{TableNodes.ATTR_STUBCOLS.value}\n" \
+    table_head = f"{Nodes.TABLE_INIT.value}\n" \
+        f"{Nodes.ATTR_HEADERROWS.value}\n" \
+        f"{Nodes.ATTR_STUBCOLS.value}\n" \
         "\n"
 
     output = table_head
@@ -45,10 +76,10 @@ def render_table(json_data: str) -> str:
 
 
     for k in keys:
-        title = "{}{}\n".format(TableNodes.FIRST_ITEM.value,k)
+        title = "{}{}\n".format(Nodes.STUB_ITEM.value,k)
         val = handle_newlines(data.get(k))
 
-        item = f"{TableNodes.ITEM.value}{val}\n\n"
+        item = f"{Nodes.ITEM.value}{val}\n\n"
         output = output + title + item
 
     return output
@@ -64,7 +95,7 @@ def handle_newlines(data: str) -> str:
         if strlist.index(line) == 0:
             output = line + "\n"
         else:
-            output = output + TableNodes.LEFTPAD.value + line + "\n"
+            output = output + Nodes.LEFTPAD.value + line + "\n"
 
     return output
 
