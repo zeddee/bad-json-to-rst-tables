062685b256d5318c6dc4ac9841466a712a82d5eb
Zed Tan <zed@shootbird.work>
2021-03-16 20:20:32 +0100

    initial implementation: handling rich content

--- json2rst/__main__.py
+++ json2rst/__main__.py
@@ -26,6 +26,7 @@ TODO:
 - ✅ Set filename write to from command line args.
 - By default, write to files named after the ``ID`` key in JSON. This is to capture the SA name as the filename.
 - ✅ Write proper example.json file.
+- Write example.json file for rich content
 - Write tests
 - ✅ Take list of files or a dir from command line args.
 - allow inserting images using base64 uri? e.g. ``.. |image2| image:: data:image/png;base64,iVBORw0KGgoAAAANSUhEU<lots more>``
@@ -36,7 +37,7 @@ import json
 import os
 from pathlib import Path
 from enum import Enum
-from typing import Tuple, List
+from typing import Tuple, List, Dict
 
 class Nodes(Enum):
     """Enum type for rst nodes"""
@@ -50,6 +51,114 @@ class Nodes(Enum):
 they match the indentation of their parent table nodes.
 Works only if the root table is at col 0
 (told ya this is an inelegant repo)."""
+    UL_ITEM = '- '
+    OL_ITEM = '#.  '
+    # CODE #: Omit. Because we need to surround the content of
+    # <code> nodes with double backticks, as opposed to prefixing
+    # the node (like with the other Nodes here)
+    CODE_BLOCK_INIT = '..  code-block::'
+    CODE_BLOCK_PAD = '    '
+    IMAGE = '..  image:: '
+
+class RichNode:
+    RICH_NODE_TYPES = [
+        "h1",
+        "h2",
+        "h3",
+        "p",
+        "ul-item",
+        "ol-item",
+        "code",
+        "code-block-item",
+        "image",
+    ]
+
+    def __init__(self, node_type: str, content: str) -> None:
+        self.content = handle_newlines(content)
+        self.n = node_type
+        
+        self._is_rich_node()
+
+    def parse(self) -> str:
+        return f"{self._handle_node()}\n\n"
+
+    def _is_rich_node(self) -> bool:
+        if self.n not in self.RICH_NODE_TYPES:
+            print(f"Invalid node type: {node_type}: {content}.\n" \
+                  "JSON key should be one of {self.RICH_NODE_TYPES}")
+            exit(1) # TODO: exception handling.
+            # return False
+
+        return True
+
+    def _handle_node(self) -> str:
+        """
+        Would *really* love to have Python 3.10 pattern matching
+        syntax here right now :(
+
+        Args:
+            n (str): Node type. Must be one of self.RICH_NODE_TYPES.
+        Returns:
+            An rST-formatted string ready to plug into our output file.
+        """
+
+
+        if self.n == "h1":
+            H1_rst = "*" * (len(self.content) + 2) # Make sure that heading marker is slightly longer than len(self.content)
+            return f"{self.content}\n{H1_rst}"
+
+        elif self.n == "h2":
+            H2_rst = "=" * (len(self.content) + 2) # Make sure that heading marker is slightly longer than len(self.content)
+            return f"{self.content}\n{H2_rst}"
+
+        elif self.n == "h2":
+            H3_rst = "-" * (len(self.content) + 2) # Make sure that heading marker is slightly longer than len(self.content)
+            return f"{self.content}\n{H3_rst}"
+
+        elif self.n == "p":
+            return self.content
+
+        elif self.n == "ul-item":
+            return f"{Nodes.UL_ITEM.value}{self.content}"
+
+        elif self.n == "ol-item":
+            return f"{Nodes.OL_ITEM.value}{self.content}"
+
+        elif self.n == "code":
+            return f"``{self.content}``"
+
+        elif self.n == "code-block-item":
+            # this needs more complex handling. Need to know if there are multiple code-block-items in sequence.
+            pass
+
+        elif self.n == "image":
+            # might need to embed as base64 image.
+            return f"{Nodes.IMAGE.value}{content}"
+
+    def _code_block_start() -> str:
+        return f"{Nodes.CODE_BLOCK_INIT.value}\n\n"
+
+    def _code_block_body() -> str:
+        return f"{Nodes.CODE_BLOCK_PAD.value}{self.content}"
+
+
+def handle_rich_content(rich_content: Dict[str, str]) -> str:
+    """
+    Args:
+        rich_content (Dict[str, str]): A dictionary of rich content nodes.
+    """
+    if not isinstance(rich_content, dict):
+        # TODO: handle exception
+        exit(1)
+
+    keys = rich_content.keys()
+    output = ""
+
+    for key in keys:
+        output = output + RichNode(key, rich_content.get(key)).parse()
+
+    return output
+
 
 def render_table(json_data: str) -> str:
     """
@@ -70,6 +179,7 @@ def render_table(json_data: str) -> str:
     data = json.loads(json_data)
 
     if isinstance(data, list):
+        # TODO: to handle JSON strings wrapped in a list. e.g. ``[{"key": "value"}]``
         pass
 
     if isinstance(data, dict):
@@ -78,9 +188,14 @@ def render_table(json_data: str) -> str:
 
     for k in keys:
         title = "{}{}\n".format(Nodes.STUB_ITEM.value,k)
-        val = handle_newlines(data.get(k))
+        val = data.get(k)
 
-        item = f"{Nodes.ITEM.value}{val}\n\n"
+        if isinstance(val, dict):
+            parsed_val = handle_rich_content(val)
+        else:
+            parsed_val = handle_newlines(val)
+
+        item = f"{Nodes.ITEM.value}{parsed_val}\n\n"
         output = output + title + item
 
     return output
